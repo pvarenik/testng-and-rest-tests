f844004019b99ac446a3a4a11ba9f31bd86a6079
pvarenik <varenikpavel@gmail.com>
2023-06-01 01:45:17 +0200

    Some fixes

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -4,8 +4,11 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.List;
 
 public class GooglePage {
@@ -13,7 +16,7 @@ public class GooglePage {
     WebDriver driver;
     By searchField = By.name("q");
     By searchButton = By.name("btnK");
-    By results = By.xpath("//br/following-sibling::h3");
+    By results = By.cssSelector("a > h3");
 
     public GooglePage(WebDriver driver) {
         this.driver = driver;
@@ -21,7 +24,7 @@ public class GooglePage {
 
     public void closeCookiesWindow() {
         List<WebElement> resultsElements = driver.findElements(By.cssSelector("div[role='none']"));
-        resultsElements.get(resultsElements.size() - 2).click();
+        resultsElements.get(resultsElements.size() - 1).click();
     }
 
     public List<WebElement> resultsElements() {
@@ -41,7 +44,8 @@ public class GooglePage {
     }
 
     public void printResults() {
-        List<WebElement> aasd = driver.findElements(results);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(results));
         for (int i = 0; i < driver.findElements(results).size(); i++) {
             WebElement result = driver.findElements(results).get(i);
             if (!result.getText().isEmpty()) {
