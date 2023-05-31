afb6272fd4f2d5abc4996bc08459d1b75b8921f7
pvarenik <varenikpavel@gmail.com>
2023-05-31 19:37:28 +0200

    Initial commit

---
+++ src/test/java/org/example/RESTfulAPIDemo.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class RESTfulAPIDemo {
+
+    @Test
+    public void verifyUser() {
+        RestAssured.baseURI = "https://api.publicapis.org";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/entries");
+        JsonPath body = response.jsonPath();
+        List<HashMap> entries = body.get("entries");
+        int numberOfAuth = 0;
+        for (HashMap entry : entries) {
+            if (entry.get("Category").toString().equals("Authentication & Authorization")) {
+                numberOfAuth += 1;
+                System.out.println(entry);
+            }
+        }
+        Assert.assertTrue(body.getInt("count") > numberOfAuth, "Number of specific entities is bigger than counted number in response" + numberOfAuth);
+    }
+}
---
+++ src/test/java/pages/GooglePage.java
@@ -0,0 +1,65 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class GooglePage {
+    static Logger logger = Logger.getLogger(GooglePage.class.getName());
+    WebDriver driver;
+    By searchField = By.name("q");
+    By searchButton = By.name("btnK");
+    By results = By.xpath("//br/following-sibling::h3");
+
+    public GooglePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void closeCookiesWindow() {
+        List<WebElement> resultsElements = driver.findElements(By.cssSelector("div[role='none']"));
+        resultsElements.get(resultsElements.size() - 2).click();
+    }
+
+    public List<WebElement> resultsElements() {
+        return driver.findElements(results);
+    }
+
+    public void enterSearchString(String str) {
+        driver.findElement(searchField).sendKeys(str);
+    }
+
+    public void clickSearchButton() {
+        for (WebElement e : driver.findElements(searchButton)) {
+            if (e.isDisplayed()) {
+                e.click();
+            }
+        }
+    }
+
+    public void printResults() {
+        List<WebElement> aasd = driver.findElements(results);
+        for (int i = 0; i < driver.findElements(results).size(); i++) {
+            WebElement result = driver.findElements(results).get(i);
+            if (!result.getText().isEmpty()) {
+                System.out.println(result.getText());
+//                logger.info(result.getText());
+            }
+        }
+    }
+
+    public void verifyResultContain(int num, String text) {
+        Assert.assertTrue(driver.findElements(results).get(num).getText().contains(text));
+    }
+
+    public void verifyResultNotContain(int num, String text) {
+        Assert.assertFalse(driver.findElements(results).get(num).getText().contains(text));
+    }
+
+    public void verifyLastResultNotContain(String text) {
+        Assert.assertFalse(driver.findElements(results).get(resultsElements().size() - 1).getText().contains(text));
+    }
+}
